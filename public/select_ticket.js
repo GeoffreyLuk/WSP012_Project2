ccb6b5563a6e0156a7dc8fae412eb7853e374230
Gary Kong <garygarylmao@gmail.com>
2023-01-28 21:06:31 +0800

    loadTickets function

--- public/select_ticket.js
+++ public/select_ticket.js
@@ -7,6 +7,7 @@ let organiserElem = document.querySelector('.organiser')
 let locationBtnElem = document.querySelector('#location > option')
 let eventDateElem = document.querySelector('.event-date-selector')
 let ticketTypeElem = document.querySelector('.ticket-type-selector')
+let ticketContainerElem = document.querySelector('.ticket-container')
 
 window.onload = () => {
     loadShowInfo()
@@ -50,7 +51,7 @@ async function loadTicketsDetails(ticketsInfo) {
             ticketTypes.push(ticketsInfo[i].ticket_type)
         }
     };
-    console.log("ticketTypes: ", ticketTypes);
+    // console.log("ticketTypes: ", ticketTypes);
 
     let eventDates = [];
     for (let i = 0; i < ticketsInfo.length; i++) {
@@ -58,13 +59,14 @@ async function loadTicketsDetails(ticketsInfo) {
             eventDates.push(ticketsInfo[i].show_date)
         }
     }
-    console.log("eventDates: ", eventDates);
+    // console.log("eventDates: ", eventDates);
 
     for (let eventDate of eventDates) {
         const opt = document.createElement('option')
         opt.text = eventDate
         eventDateElem.appendChild(opt)
     };
+
     for (let ticketType of ticketTypes) {
         const opt = document.createElement('option')
         opt.text = ticketType
@@ -74,4 +76,35 @@ async function loadTicketsDetails(ticketsInfo) {
 
 async function loadTickets(ticketsInfo) {
     console.log("ticketsInfo: ", ticketsInfo);
+    ticketContainerElem.innerHTML = ''
+    for (let ticketInfoItem of ticketsInfo) {
+        let timeFormat = new Date(ticketInfoItem.show_date)
+        let date = timeFormat.toDateString()
+        let time = timeFormat.getHours() + ":" + timeFormat.getMinutes()
+        let div = document.createElement('div')
+        div.classList.add('ticket')
+        let div1 = document.createElement('div')
+        div1.classList.add('show-date')
+        div1.innerText = date
+        div.appendChild(div1);
+        let div2 = document.createElement('div')
+        div2.classList.add('show-time')
+        div2.innerText = time
+        div.appendChild(div2);
+        let div3 = document.createElement('div')
+        div3.classList.add('ticket-type')
+        div3.innerText = ticketInfoItem.ticket_type
+        div.appendChild(div3);
+        let div4 = document.createElement('div')
+        div4.classList.add('ticket-price')
+        div4.innerText = "$" + ticketInfoItem.pricing
+        div.appendChild(div4);
+        let select = document.createElement('select')
+        select.classList.add('ticket-quantity')
+        div.appendChild(select);
+        let opt = document.createElement('option')
+        opt.innerText = 1
+        select.appendChild(opt);
+        ticketContainerElem.appendChild(div)
+    }
 }
