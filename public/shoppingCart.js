86dc619f6326c4b098d472339b239b545fea7b17
Gary Kong <garygarylmao@gmail.com>
2023-01-30 18:37:49 +0800

    finished shoppingCart functions

--- public/shoppingCart.js
+++ public/shoppingCart.js
@@ -1,19 +1,31 @@
 let show = document.URL.split('/show_').pop()
 let showContainerElem = document.querySelector('.show-container')
 let totalAmountElem = document.querySelector('.total-amount')
+let checkboxElem = document.querySelector('.check-out-form').elements
+let checkoutBtnElem = document.querySelector('.checkout-btn')
+let ifIncludesShow = window.location.toString().includes("show_")
+let alterElem = document.querySelector('.check-out-item > a')
 
+
+console.log("URL to string: ", ifIncludesShow);
 console.log("show: ", show);
 
 async function init() {
     getUserInfo()
-    console.log("init()");
+    // console.log("init()");
     loadCheckout()
+    alterCheckoutBtn()
 }
 
 async function loadCheckout() {
-    let res = await fetch(`/get_checkout/${show}`)
+    let res
+    if (ifIncludesShow == false) {
+        res = await fetch(`/get_checkout`)
+    } else {
+        res = await fetch(`/get_checkout/${show}`)
+    }
     if (res.ok) {
-        console.log("loadCheckout res.ok");
+        // console.log("loadCheckout res.ok");
         let data = await res.json()
         let ticketsInfo = data.ticketsInfo
         let showsInfo = data.showsInfo
@@ -32,74 +44,144 @@ async function getUserInfo() {
 }
 
 async function loadTicketsByShowId(ticketsInfo, showsInfo) {
+    let sumTotal = 0
     // console.log("ticketsInfo: ", ticketsInfo)
-    // console.log("showsInfo: ", showsInfo);
-    // console.log("showsInfo.length: ", showsInfo.length);
-    // console.log(typeof (showsInfo));
+    showContainerElem.innerHTML = ''
     if (ticketsInfo.length > 0) {
-        showContainerElem.innerHTML = ''
-        const event = document.createElement('div')
-        event.classList.add('event')
-        showContainerElem.appendChild(event)
-        const div1 = document.createElement('div')
-        div1.classList.add('show-info')
-        event.appendChild(div1)
-        const delBtn = document.createElement('a')
-        delBtn.classList.add('delete')
-        delBtn.onclick = function () {
-            deleteAllShowTicketsByShowName(showsInfo.organiser_name)
-        }
-        delBtn.innerText = "deleteAllShowTicketsByShowName"
-        div1.appendChild(delBtn)
-        const div2 = document.createElement('div')
-        div2.classList.add('organiser-name')
-        div2.innerText = showsInfo.organiser_name
-        div1.appendChild(div2)
-        const div3 = document.createElement('div')
-        div3.classList.add('show-name')
-        div3.innerText = showsInfo.show_name
-        div1.appendChild(div3)
-        let sumTotal = 0
-        for (let ticketsItem of ticketsInfo) {
-            let timeFormat = new Date(ticketsItem.show_date)
-            let time = timeFormat.toDateString() + " / " + timeFormat.getHours() + ":" + timeFormat.getMinutes()
-
-
-            const ticketContainer = document.createElement('div');
-            ticketContainer.classList.add('ticket-container')
-            event.appendChild(ticketContainer);
-            const ticket = document.createElement('div');
-            ticket.classList.add('ticket')
-            ticketContainer.appendChild(ticket)
+        if (ifIncludesShow == false) {
+            console.log("False");
+            console.log("ticketsInfo: ", ticketsInfo);
+            // console.log("showsInfo: ", showsInfo);
+            for (let showsItem of showsInfo) {
+                console.log("showsItem: ", showsItem);
+                const event = document.createElement('div')
+                event.classList.add('event')
+                showContainerElem.appendChild(event)
+                event.setAttribute("id", "show_" + showsItem.show_id)
+                const showInfo = document.createElement('div')
+                showInfo.classList.add('show-info')
+                event.appendChild(showInfo)
+                const delBtn = document.createElement('a')
+                delBtn.classList.add('delete')
+                delBtn.onclick = function () {
+                    deleteAllShowTicketsByShowName(showsItem.organiser_name)
+                }
+                delBtn.innerText = "deleteAllShowTicketsByShowName"
+                showInfo.appendChild(delBtn)
+                const organiserName = document.createElement('div')
+                organiserName.classList.add('organiser-name')
+                organiserName.innerText = showsItem.organiser_name
+                showInfo.appendChild(organiserName)
+                const showName = document.createElement('div')
+                showName.classList.add('show-name')
+                showName.innerText = showsItem.show_name
+                showInfo.appendChild(showName)
+            }
+            for (let ticketsItem of ticketsInfo) {
+                console.log("Run ticketsInfo Loop");
+                for (i = 0; i < showContainerElem.childNodes.length; i++) {
+                    console.log("showContainerElem.childNodes[i].id:", showContainerElem.childNodes[i].id);
+                    if (("show_" + ticketsItem.show_id) == showContainerElem.childNodes[i].id) {
+                        let eventId = showContainerElem.childNodes[i].id
+                        let event = document.querySelector(`#${eventId}`)
+                        console.log("eventId", eventId);
+                        let timeFormat = new Date(ticketsItem.show_date)
+                        let time = timeFormat.toDateString() + " / " + timeFormat.getHours() + ":" + timeFormat.getMinutes()
+                        console.log("time: ", time);
+                        const ticketContainer = document.createElement('div');
+                        ticketContainer.classList.add('ticket-container')
+                        event.appendChild(ticketContainer);
+                        const ticket = document.createElement('div');
+                        ticket.classList.add('ticket')
+                        ticketContainer.appendChild(ticket)
+                        const delBtn = document.createElement('a')
+                        delBtn.classList.add('delete')
+                        delBtn.onclick = function () {
+                            deleteShowTicket(ticketsItem.users_purchases_id)
+                        }
+                        delBtn.innerText = "deleteShowTicket"
+                        ticket.appendChild(delBtn)
+                        const ticketType = document.createElement('div');
+                        ticketType.classList.add('ticket-type')
+                        ticketType.innerText = ticketsItem.type
+                        ticket.appendChild(ticketType)
+                        const ticketPrice = document.createElement('div');
+                        ticketPrice.classList.add('ticket-price')
+                        ticketPrice.innerText = "$" + ticketsItem.pricing
+                        ticket.appendChild(ticketPrice)
+                        const ticketDate = document.createElement('div');
+                        ticketDate.classList.add('ticket-date')
+                        ticketDate.innerText = time
+                        ticket.appendChild(ticketDate)
+                        const ticketVenue = document.createElement('div');
+                        ticketVenue.classList.add('ticket-venue')
+                        ticketVenue.innerText = ticketsItem.venue
+                        ticket.appendChild(ticketVenue)
+                        sumTotal += ticketsItem.pricing
+                        totalAmountElem.innerText = "$" + sumTotal
+                    }
+                }
+            }
+        } else {
+            const event = document.createElement('div')
+            event.classList.add('event')
+            showContainerElem.appendChild(event)
+            const div1 = document.createElement('div')
+            div1.classList.add('show-info')
+            event.appendChild(div1)
             const delBtn = document.createElement('a')
             delBtn.classList.add('delete')
             delBtn.onclick = function () {
-                deleteShowTicket(ticketsItem.type, timeFormat)
+                deleteAllShowTicketsByShowName(showsInfo.organiser_name)
             }
-            delBtn.innerText = "deleteShowTicket"
-            ticket.appendChild(delBtn)
-            const ticketType = document.createElement('div');
-            ticketType.classList.add('ticket-type')
-            ticketType.innerText = ticketsItem.type
-            ticket.appendChild(ticketType)
-            const ticketPrice = document.createElement('div');
-            ticketPrice.classList.add('ticket-price')
-            ticketPrice.innerText = "$" + ticketsItem.pricing
-            ticket.appendChild(ticketPrice)
-            const ticketDate = document.createElement('div');
-            ticketDate.classList.add('ticket-date')
-            ticketDate.innerText = time
-            ticket.appendChild(ticketDate)
-            const ticketVenue = document.createElement('div');
-            ticketVenue.classList.add('ticket-venue')
-            ticketVenue.innerText = showsInfo.venue
-            ticket.appendChild(ticketVenue)
-
-            sumTotal += ticketsItem.pricing
-            totalAmountElem.innerText = "$" + sumTotal
-
+            delBtn.innerText = "deleteAllShowTicketsByShowName"
+            div1.appendChild(delBtn)
+            const div2 = document.createElement('div')
+            div2.classList.add('organiser-name')
+            div2.innerText = showsInfo.organiser_name
+            div1.appendChild(div2)
+            const div3 = document.createElement('div')
+            div3.classList.add('show-name')
+            div3.innerText = showsInfo.show_name
+            div1.appendChild(div3)
+            for (let ticketsItem of ticketsInfo) {
+                let timeFormat = new Date(ticketsItem.show_date)
+                let time = timeFormat.toDateString() + " / " + timeFormat.getHours() + ":" + timeFormat.getMinutes()
+                const ticketContainer = document.createElement('div');
+                ticketContainer.classList.add('ticket-container')
+                event.appendChild(ticketContainer);
+                const ticket = document.createElement('div');
+                ticket.classList.add('ticket')
+                ticketContainer.appendChild(ticket)
+                const delBtn = document.createElement('a')
+                delBtn.classList.add('delete')
+                delBtn.onclick = function () {
+                    deleteShowTicket(ticketsItem.users_purchases_id)
+                }
+                delBtn.innerText = "deleteShowTicket"
+                ticket.appendChild(delBtn)
+                const ticketType = document.createElement('div');
+                ticketType.classList.add('ticket-type')
+                ticketType.innerText = ticketsItem.type
+                ticket.appendChild(ticketType)
+                const ticketPrice = document.createElement('div');
+                ticketPrice.classList.add('ticket-price')
+                ticketPrice.innerText = "$" + ticketsItem.pricing
+                ticket.appendChild(ticketPrice)
+                const ticketDate = document.createElement('div');
+                ticketDate.classList.add('ticket-date')
+                ticketDate.innerText = time
+                ticket.appendChild(ticketDate)
+                const ticketVenue = document.createElement('div');
+                ticketVenue.classList.add('ticket-venue')
+                ticketVenue.innerText = showsInfo.venue
+                ticket.appendChild(ticketVenue)
+                sumTotal += ticketsItem.pricing
+                totalAmountElem.innerText = "$" + sumTotal
+            }
         }
-        console.log("sumTotal: ", sumTotal);
+
+        // console.log("sumTotal: ", sumTotal);
     }
 }
 
@@ -118,13 +200,72 @@ async function deleteAllShowTicketsByShowName(organiserName) {
     }
 }
 
-async function deleteShowTicket(type, showDate) {
-    console.log("type: ", type);
-    console.log("showDate ", showDate);
-    let eventDate = showDate + showDate.setHours((showDate.getHours() + 8))
-    console.log(eventDate);
-    let eventTimestamp = new Date(eventDate)
-    console.log("timestamp: ", eventTimestamp);
+async function deleteShowTicket(selectedTicketId) {
+    console.log("selectedTicketId: ", selectedTicketId);
+    let data = { selectedTicketId }
+    let res = await fetch('/delete_ticket', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    if (res.ok) {
+        console.log("deleteShowTicket works");
+    }
+}
+
+function ifChecked() {
+    if (!checkboxElem.agree.checked) {
+        console.log("No check");
+        return false
+    } else {
+        return true
+    }
+}
+
+checkoutBtnElem.addEventListener('click', (e) => {
+    e.preventDefault()
+    console.log("Clicked");
+    if (ifChecked()) {
+        proceedPayment()
+    } else {
+        return
+    }
+})
+
+async function proceedPayment() {
+    let res
+    if (ifIncludesShow == false) {
+        console.log("No show id");
+        res = await fetch(`/proceed_purchase`)
+    } else {
+        console.log("Show ID");
+        let data = { show }
+        res = await fetch(`/proceed_purchase/${show}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        console.log(data);
+    }
+    if (res.ok) {
+        let data = await res.json()
+        console.log("proceedPayment data: ", data);
+    } else {
+        console.log("failed");
+    }
+}
+
+function alterCheckoutBtn() {
+    if (ifIncludesShow == false) {
+        alterElem.setAttribute("href", "/proceed_purchase")
+    } else {
+        alterElem.setAttribute("href", `/proceed_purchase/${show}`)
+    }
+    return
 }
 
 init()
--- shoppingCartRoutes.ts
+++ shoppingCartRoutes.ts
@@ -4,35 +4,46 @@ import { client } from "./database/init_data";
 
 export const shoppingCartRoutes = express.Router()
 
-shoppingCartRoutes.get('/checkout/:show_id', directToCheckout)
-shoppingCartRoutes.get('/get_checkout/:show_id', getCheckoutInfo)
-shoppingCartRoutes.post('/delete_tickets_by_name', deleteAllTicketsByName)
+shoppingCartRoutes.get('/checkout/:show_id', directToCheckout) //Done
+shoppingCartRoutes.get('/get_checkout/:show_id', getCheckoutInfo) //Done
+shoppingCartRoutes.post('/delete_tickets_by_name', deleteAllTicketsByName) //Done
+shoppingCartRoutes.post('/delete_ticket/', deleteTicket) //Done
+shoppingCartRoutes.get('/checkout', directToCheckoutWithNoShowId) //Done
+shoppingCartRoutes.get('/get_checkout', getCheckoutInfoWithNoShowId)//Done
+shoppingCartRoutes.post('/proceed_purchase/:show_id', proceedPurchaseWithShowId)//Done
+shoppingCartRoutes.get('/proceed_purchase', proceedPurchase)//Done
+
+
+async function directToCheckoutWithNoShowId(req: express.Request, res: express.Response) {
+    res.sendFile(path.join(__dirname, 'public', 'shoppingCart.html'))
+}
 
 async function directToCheckout(req: express.Request, res: express.Response) {
     res.sendFile(path.join(__dirname, 'public', 'shoppingCart.html'))
 }
 
 async function getCheckoutInfo(req: express.Request, res: express.Response) {
-    console.log("getCheckoutInfo");
-    // let userId = req.session["user"]["id"]
-    let userId = 21
-    let showId = req.params.show_id
-    console.log("showId: ", showId);
+    try {
+        // console.log("getCheckoutInfo");
+        // let userId = req.session["user"]["id"]
+        let userId = 21
+        let showId = req.params.show_id
+        console.log("showId: ", showId);
 
-    let ticketPurchaseResults = await client.query(
-        `select type, pricing, show_date from users_purchases
+        let ticketPurchaseResults = await client.query(
+            `select users_purchases.id as users_purchases_id, type, pricing, show_date from users_purchases
             inner join tickets
             on tickets.id = ticket_id
             where show_id = $1 and user_id = $2 and ticket_paid = false`,
-        [showId, userId]
-    )
-    let ticketsInfo = ticketPurchaseResults.rows
+            [showId, userId]
+        )
+        let ticketsInfo = ticketPurchaseResults.rows
 
-    console.log("ticketsInfo: ", ticketsInfo);
+        console.log("ticketsInfo: ", ticketsInfo);
 
 
-    let showInfoResult = await client.query(
-        `
+        let showInfoResult = await client.query(
+            `
         select show_name, organiser_name, venue from shows
             inner join organiser_list
     	        on organiser_id = organiser_list.id
@@ -42,27 +53,98 @@ async function getCheckoutInfo(req: express.Request, res: express.Response) {
             	on locations.id = location_id
         where shows.id = $1
         `,
-        [showId]
-    )
+            [showId]
+        )
 
-    let showsInfo = showInfoResult.rows[0]
-    console.log("showsInfo: ", showsInfo);
+        let showsInfo = showInfoResult.rows[0]
+        console.log("showsInfo: ", showsInfo);
 
-    res.json({
-        ticketsInfo,
-        showsInfo,
-        message: "haha"
-    })
+        res.json({
+            ticketsInfo,
+            showsInfo,
+            message: "haha"
+        })
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: '[SCR001] - Server error'
+        })
+    }
+
 
 }
 
-async function deleteAllTicketsByName(req: express.Request, res: express.Response) {
-    let data = req.body
-    // Hardcode
-    let userId = 21
+async function getCheckoutInfoWithNoShowId(req: express.Request, res: express.Response) {
+    try {
+        // Hardcode
+        let userId = 21
 
-    let delTicketsIdResults = await client.query(
-        `
+        let ticketPurchaseResults = await client.query(
+            `
+        select shows.id as show_id, users_purchases.id as users_purchases_id, type, pricing, show_date, venue from users_purchases
+            inner join tickets
+            on tickets.id = ticket_id
+            inner join shows
+            on shows.id = show_id
+            inner join shows_locations 
+			on shows_locations.show_id = shows.id 
+			inner join locations
+			on locations.id =shows_locations.location_id
+			where user_id = $1 and ticket_paid = false`,
+            [userId]
+        )
+
+        let showIdResults = await client.query(
+            `
+        select distinct show_id from users_purchases
+            inner join tickets
+            on tickets.id = ticket_id
+            where user_id = $1 and ticket_paid = false`,
+            [userId]
+        )
+        let ticketsInfo = ticketPurchaseResults.rows
+        let showIdsInfo = showIdResults.rows
+        let showsInfo = []
+        for (let showId of showIdsInfo) {
+            // console.log("showId.show_id: ", showId.show_id);
+            let showInfoResults = (await client.query(
+                `
+                select show_id, show_name, organiser_name, venue from shows
+            inner join organiser_list
+        	    on organiser_id = organiser_list.id
+            inner join shows_locations 
+        	    on show_id = shows.id
+            inner join locations
+            	on locations.id = location_id
+            where shows.id = $1
+            `, [showId.show_id]
+            )).rows[0]
+            showsInfo.push(showInfoResults)
+        }
+        // console.log("showInfo: ", showsInfo);
+        // console.log("ticketsInfo: ", ticketsInfo);
+
+        res.json({
+            ticketsInfo,
+            showsInfo,
+            message: "getCheckoutInfoWithNoShowId Done"
+        })
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: '[SCR005] - Server error'
+        })
+    }
+}
+
+async function deleteAllTicketsByName(req: express.Request, res: express.Response) {
+    try {
+        let data = req.body
+        // Hardcode
+        let userId = 21
+
+        let delTicketsIdResults = await client.query(
+            `
         select distinct ticket_id from users_purchases
         	inner join tickets
         		on ticket_id = tickets.id
@@ -72,19 +154,105 @@ async function deleteAllTicketsByName(req: express.Request, res: express.Respons
         		on	organiser_id = organiser_list.id
         	where users_purchases.user_id = $1 and ticket_paid = false and organiser_name = $2
         `, [userId, data.organiserName]
-    )
+        )
 
-    // console.log("delTicketsIdResults: ", delTicketsIdResults.rows);
-    let delTicketIds = delTicketsIdResults.rows
-    console.log("delTicketIds: ", delTicketIds);
+        // console.log("delTicketsIdResults: ", delTicketsIdResults.rows);
+        let delTicketIds = delTicketsIdResults.rows
+        console.log("delTicketIds: ", delTicketIds);
 
-    for (let delTicketId of delTicketIds) {
-        console.log("delTicketId.ticket_id: ", delTicketId.ticket_id)
+        for (let delTicketId of delTicketIds) {
+            console.log("delTicketId.ticket_id: ", delTicketId.ticket_id)
+            await client.query(
+                `
+            delete from users_purchases where user_id = $1 and ticket_id = $2 and ticket_paid = false`,
+                [userId, delTicketId.ticket_id]
+            )
+        }
+        res.json({ message: 'delete tickets ok' })
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: '[SCR002] - Server error'
+        })
+    }
+}
+
+async function deleteTicket(req: express.Request, res: express.Response) {
+    try {
+        console.log("Hi");
+        let { selectedTicketId } = req.body
+        console.log("selectedTicketId: ", selectedTicketId);
+        // console.log("eventTimestamp: ", eventTimestamp);
+        await client.query(
+            `delete from users_purchases where id = $1`, [selectedTicketId]
+        )
+        res.json({
+            message: 'delete ticket done'
+        })
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: '[SCR003] - Server error'
+        })
+    }
+
+}
+
+async function proceedPurchaseWithShowId(req: express.Request, res: express.Response) {
+    try {
+        console.log("proceedPurchaseWithShowId");
+        let showId = req.params.show_id
+        // hardcode
+        let userId = 21
+
         await client.query(
             `
-            delete from users_purchases where user_id = $1 and ticket_id = $2 and ticket_paid = false`,
-            [userId, delTicketId.ticket_id]
+        UPDATE users_purchases
+        SET ticket_paid = true
+        FROM users_purchases as up
+        	inner join tickets
+        		on tickets.id = ticket_id 
+        where up.user_id = $1
+        	and up.ticket_paid = false 
+        	and show_id = $2
+        `, [userId, showId]
         )
+
+        res.json({
+            message: "proceedPurchaseWithShowId Done"
+        })
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: '[SCR004] - Server error'
+        })
     }
-    res.json({ message: 'delete tickets ok' })
+}
+
+async function proceedPurchase(req: express.Request, res: express.Response) {
+    try {
+        console.log("proceedPurchase");
+        // Hardcode
+        let userId = 21
+
+        await client.query(
+            `
+        UPDATE users_purchases
+        SET ticket_paid = true
+        where user_id = $1
+        	and ticket_paid = false 
+        `, [userId]
+        )
+        console.log("This is working fine");
+
+        res.json({
+            message: "proceedPurchase Done"
+        })
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: '[SCR005] - Server error'
+        })
+    }
+
 }
